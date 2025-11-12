b308a182b9b7ad57644dfe841ee655dcbff07dce
ayushi pal <ayushipal06@gmail.com>
2025-11-12 16:17:21 +0530

    completed about us section

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>
+      <body className='bg-[#CBD99B]'>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
